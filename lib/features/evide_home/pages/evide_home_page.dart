360c12dcd01841788091c05475a398173a25ae9c
DUDU0123 <sdu200115@gmail.com>
2025-09-10 01:44:18 +0530

    just added one simple state change when location icon clicked

--- lib/features/evide_home/pages/evide_home_page.dart
+++ lib/features/evide_home/pages/evide_home_page.dart
@@ -4,10 +4,17 @@ import 'package:evide_user/features/evide_home/widgets/home_page_search_bar.dart
 import 'package:evide_user/features/evide_home/widgets/recent_route_widget.dart';
 import 'package:flutter_svg/svg.dart';
 
-class EvideHomePage extends StatelessWidget {
+class EvideHomePage extends StatefulWidget {
   EvideHomePage({super.key});
 
+  @override
+  State<EvideHomePage> createState() => _EvideHomePageState();
+}
+
+class _EvideHomePageState extends State<EvideHomePage> {
   TextEditingController searchController = TextEditingController();
+
+  bool isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -33,15 +40,36 @@ class EvideHomePage extends StatelessWidget {
                 fontWeight: FontWeight.w600,
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(8.r),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.amber,
-              ),
-              child: Image.asset(
-                AppAssets.locationIcon,
-                height: 20,
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  isExpanded = !isExpanded;
+                });
+              },
+              child: Container(
+                padding: EdgeInsets.all(8.r),
+                decoration: BoxDecoration(
+                  // shape: BoxShape.circle,
+                  borderRadius: BorderRadius.circular(30.sp),
+                  color: Colors.amber,
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      AppAssets.locationIcon,
+                      height: isExpanded ? 15 : 20,
+                    ),
+                    if (isExpanded)
+                      Text(
+                        "Kerala, India",
+                        style: AppCommonStyles.commonTextStyle(
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: AppAssets.belanosima,
+                        ),
+                      )
+                  ],
+                ),
               ),
             )
           ],
@@ -55,12 +83,15 @@ class EvideHomePage extends StatelessWidget {
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
-              children: List.generate(10, (index) {
-                return Padding(
-                  padding: EdgeInsets.only(left: 5.w, right: 5.w),
-                  child: RecentRouteWidget(),
-                );
-              },),
+              children: List.generate(
+                10,
+                (index) {
+                  return Padding(
+                    padding: EdgeInsets.only(left: 5.w, right: 5.w),
+                    child: RecentRouteWidget(),
+                  );
+                },
+              ),
             ),
           ),
           // nearby stops navigate part
